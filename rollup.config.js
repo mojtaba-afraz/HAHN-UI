7b9157c2f6ed1422b85df40917e10f2d90a75759
mojtaba afraz <mojtabaafraz1@gmail.com>
2022-08-24 00:42:10 +0430

    add rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+
+import summary from 'rollup-plugin-summary';
+import {terser} from 'rollup-plugin-terser';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+
+export default {
+  input: 'dist/compiled/index.js',
+  output: {
+    file: 'dist/bundle/hahn-ui.js',
+    format: 'esm',
+  },
+  onwarn(warning) {
+    if (warning.code !== 'THIS_IS_UNDEFINED') {
+      console.error(`(!) ${warning.message}`);
+    }
+  },
+  plugins: [
+    replace({'Reflect.decorate': 'undefined'}),
+    resolve(),
+    terser({
+      ecma: 2017,
+      module: true,
+      warnings: true,
+      format: {
+        comments: false
+      },
+      compress: true,
+      mangle: {
+        properties: {
+          regex: /^__/,
+        },
+      },
+    }),
+    summary(),
+  ],
+};
