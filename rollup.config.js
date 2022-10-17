6853c61cc3bd7dfe04763f17e6acea3159bc2f81
mojtaba afraz <mojtabaafraz1@gmail.com>
2022-10-17 19:19:38 +0330

    change build directory

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
 export default {
-  input: 'dist/compiled/index.js',
+  input: 'dev/dist/compiled/index.js',
   output: {
     file: 'dist/bundle/hahn-ui.js',
     format: 'esm',
