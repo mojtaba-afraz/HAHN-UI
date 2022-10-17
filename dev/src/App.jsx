aa6412bcac0c1d05fab36f1e210c9e05322dc725
mojtaba afraz <mojtabaafraz1@gmail.com>
2022-10-17 19:30:43 +0330

    change staging to a react app

---
+++ dev/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+        <h-table>
+            <div slot="name">
+                col + hallo
+            </div>
+        </h-table>
+    </div>
+  )
+}
+
+export default App
